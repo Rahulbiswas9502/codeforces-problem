f881db2c82736cf15d098bae1f68b4496bfe1939
Rahul Biswas <104161953+Rahulbiswas9502@users.noreply.github.com>
2022-10-19 03:06:15 +0600

    Add files via upload

---
+++ A. Nearly Lucky Number_110.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    long long int z=0,num,a,i,sum,rem;
+    scanf("%lld",&a);
+    while(a>0)
+    {
+        rem=a%10;
+        if(rem==4 ||rem==7){
+            z++;
+        }
+        a=a/10;
+    }
+
+    if(z==4 ||z==7){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+
+
+
+
+
+    return 0;
+}
